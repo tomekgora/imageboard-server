f041e26e37520779afceed868a260cfcf17252cd
tomekgora <tomekgora@gmail.com>
2019-12-06 11:35:44 +0100

    added image model, imported to index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
+const db = require('./db')
+const Image = require('./image/model')
 
 app.listen(port, console.log(`We are listening on port: ${port}`))
