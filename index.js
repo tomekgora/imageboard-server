364bd406c868e61e8e3eaf9e5aee4a808b496f87
tomekgora <tomekgora@gmail.com>
2019-12-06 11:54:25 +0100

    added router

--- index.js
+++ index.js
@@ -3,5 +3,7 @@ const app = express()
 const port = process.env.PORT || 4000
 const db = require('./db')
 const Image = require('./image/model')
+const imageRouter = require('./image/router')
 
+app.use(imageRouter)
 app.listen(port, console.log(`We are listening on port: ${port}`))
